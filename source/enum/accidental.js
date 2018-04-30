8ddb79c3d388ffda7fd54156d4780dbcfbfaed5f
Nelson Tran <nutty@wha.al>
2018-04-30 11:23:15 -0700

    Add pitch enumerations

---
+++ source/enum/accidental.js
@@ -0,0 +1,9 @@
+const Accidental = Object.freeze({
+    DOUBLE_FLAT: -2,
+    FLAT: -1,
+    NATURAL: 0,
+    SHARP: 1,
+    DOUBLE_SHARP: 2
+});
+
+module.exports = Accidental;
---
+++ source/enum/letter.js
@@ -0,0 +1,11 @@
+const Letter = Object.freeze({
+    C: 0,
+    D: 2,
+    E: 4,
+    F: 5,
+    G: 7,
+    A: 9,
+    B: 11
+});
+
+module.exports = Letter;
